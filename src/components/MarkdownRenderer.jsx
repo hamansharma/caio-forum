cb4f0454435e5b097f0932d37b278f811bd7535b
echopolicy <uncmadisonapex@gmail.com>
2026-05-27 19:50:23 -0500

    fixed markdown css

--- src/components/MarkdownRenderer.jsx
+++ src/components/MarkdownRenderer.jsx
@@ -3,10 +3,18 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import './MarkdownRenderer.css';
 
+const components = {
+  table: ({ children }) => (
+    <div className="table-wrapper">
+      <table>{children}</table>
+    </div>
+  ),
+};
+
 export default function MarkdownRenderer({ content }) {
   return (
     <div className="markdown-body">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} components={components}>
         {content}
       </ReactMarkdown>
     </div>
